8e76b35aa485edd41907ce86e95982d3e8d3c416
saurabh007007 <loverboybhol143@gmail.com>
2025-10-15 23:54:36 +0530

    cat-file command added

--- src/main.cpp
+++ src/main.cpp
@@ -42,10 +42,37 @@ int main(int argc, char *argv[])
             std::cerr << e.what() << '\n';
             return EXIT_FAILURE;
         }
-    } else {
-        std::cerr << "Unknown command " << command << '\n';
-        return EXIT_FAILURE;
+    } else if (command == "cat-file") {
+    if (argc <= 3) {
+      std::cerr << "Invalid arguments, required `-p <blob-sha>`\n";
+      return EXIT_FAILURE;
     }
+    const std::string flag = argv[2];
+    if (flag != "-p") {
+      std::cerr << "Invalid flag for cat-file, expected `-p`\n";
+      return EXIT_FAILURE;
+    }
+    const std::string value = argv[3];
+    const std::string dir_name = value.substr(0, 2);
+    const std::string blob_sha = value.substr(2);
+
+    std::string path = ".git/objects/" + dir_name + "/" + blob_sha;
+    zstr::ifstream input(path, std::ofstream::binary);
+    if (!input.is_open()) {
+      std::cerr << "Failed to open object file?\n";
+      return EXIT_FAILURE;
+    }
+
+    std::string object_str{std::istreambuf_iterator<char>(input),
+                           std::istreambuf_iterator<char>()};
+    input.close();
+    const auto object_content = object_str.substr(object_str.find('\0') + 1);
+    std::cout << object_content << std::flush;
+
+  } else{
+    std::cerr << "Unknown command: " << command << "\n";
+    return EXIT_FAILURE;
+  }
     
     return EXIT_SUCCESS;
 }
